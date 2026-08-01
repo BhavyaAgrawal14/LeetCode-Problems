47a2d36383dfb499d75be8e961dea149705b53e2
Bhavya Agrawal <bhaavyaagrawal@gmail.com>
2026-08-01 12:32:26 +0530

    Time: 2 ms (43.58%), Space: 10.3 MB (32.54%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool predictTheWinner(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = nums[i];
+        }
+
+        for (int len = 2; len <= n; len++) {
+            for (int left = 0; left + len - 1 < n; left++) {
+                int right = left + len - 1;
+
+                int takeLeft = nums[left] - dp[left + 1][right];
+                int takeRight = nums[right] - dp[left][right - 1];
+
+                dp[left][right] = max(takeLeft, takeRight);
+            }
+        }
+
+        return dp[0][n - 1] >= 0;
+    }
+};
