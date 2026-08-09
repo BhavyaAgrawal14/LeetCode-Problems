02992398bcd66386118ebdfbf0fc70193c649ee7
Bhavya Agrawal <bhaavyaagrawal@gmail.com>
2026-08-09 23:28:17 +0530

    Time: 7 ms (77.21%), Space: 13.7 MB (55.9%) - LeetHub

---
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int n;
+    vector<int> suffix;
+    vector<vector<int>> dp;
+
+    int solve(int i, int M) {
+        if (i >= n)
+            return 0;
+
+        // Can take all remaining piles
+        if (i + 2 * M >= n)
+            return suffix[i];
+
+        if (dp[i][M] != -1)
+            return dp[i][M];
+
+        int ans = 0;
+
+        // Try taking X piles
+        for (int X = 1; X <= 2 * M && i + X <= n; X++) {
+
+            // Current player gets:
+            // total remaining - opponent's best
+            int opponent = solve(i + X, max(M, X));
+
+            ans = max(ans, suffix[i] - opponent);
+        }
+
+        return dp[i][M] = ans;
+    }
+
+    int stoneGameII(vector<int>& piles) {
+        n = piles.size();
+
+        // suffix[i] = total stones from i to n-1
+        suffix.assign(n + 1, 0);
+
+        for (int i = n - 1; i >= 0; i--) {
+            suffix[i] = suffix[i + 1] + piles[i];
+        }
+
+        dp.assign(n, vector<int>(n + 1, -1));
+
+        return solve(0, 1);
+    }
+};
