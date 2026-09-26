5a5a9b9a5cc1505f8280bfe28511a34f979da6ea
Bhavya Agrawal <bhaavyaagrawal@gmail.com>
2026-09-26 06:33:14 +0530

    Time: 80 ms (57.32%), Space: 122.8 MB (46.73%) - LeetHub

---
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    string evaluate(string s, vector<vector<string>>& knowledge) {
+        unordered_map<string, string> mp;
+
+        // Store key -> value
+        for (auto &x : knowledge) {
+            mp[x[0]] = x[1];
+        }
+
+        string ans;
+
+        for (int i = 0; i < s.size(); i++) {
+
+            if (s[i] != '(') {
+                ans += s[i];
+            }
+            else {
+                // Find closing bracket
+                int j = i + 1;
+                while (s[j] != ')') {
+                    j++;
+                }
+
+                // Extract key
+                string key = s.substr(i + 1, j - i - 1);
+
+                // Replace with value or '?'
+                if (mp.count(key))
+                    ans += mp[key];
+                else
+                    ans += '?';
+
+                // Skip everything up to ')'
+                i = j;
+            }
+        }
+
+        return ans;
+    }
+};
